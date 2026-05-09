7d665b53eeb331a337809e267dc915f123f0110c
SharmaAtul12 <sharmaatulas6@gmail.com>
2026-05-09 17:15:24 +0530

    revised agent-tool and create_conv.js

--- create-conv.js
+++ create-conv.js
@@ -3,6 +3,7 @@ import { OpenAI } from 'openai';
 
 const client = new OpenAI();
 
+// Creating a conversation and Logging the conversation ID
 client.conversations.create({}).then((e) => {
   console.log(`Conversation created with ID: ${e.id}`);
 })
